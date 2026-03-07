5b5a044e789f35a34583a5cb5d942b873f12d33f
Christoph Hader <christoph.hader@gmail.com>
2026-03-07 09:04:24 -0700

    fixing ruff linter flag preventing proper testing

--- src/flow_state/__init__.py
+++ src/flow_state/__init__.py
@@ -1,9 +1,10 @@
 """Compressible flow state utilities."""
 
-from flow_state import atmosphere, io
 from importlib.metadata import version
 
 __version__ = version("flow-state-calculator")
+
+from flow_state import atmosphere, io
 from flow_state.core import FlowState
 from flow_state.isentropic import stag_to_stat, stat_to_stag
 from flow_state.solvers import (
